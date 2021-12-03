3af49b4ba368bd60bc777266f02a0e32595d0be1
Larsson <riu@ssb.no>
2021-12-03 12:09:38 +0100

    cleaned up

--- 3/3.py
+++ 3/3.py
@@ -42,62 +42,16 @@ def epgam(filename):
     
     return int(gamma, 2)*int(epsilon,2)
 
-def ox(filename):
-
-    bitFile = open(filename)
 
 
-    zeroCounter = 0
-    oneCounter = 0
+def life_support_rating(filename, measure = "co2"):
 
-    bitList = []
-    updatedBitList = []
-    
-    for bitnumber in bitFile:
-
-        stripedBitnumber = bitnumber.strip()
-        bitList.append(stripedBitnumber)
-  
-  
-    for i in range(len(stripedBitnumber)):
-
-        if len(bitList) == 1:
-            return bitList
-        for j in range(len(bitList)):
-            
-            number = bitList[j][i]
-            
-            if int(number) == 1:
-                oneCounter += 1
-            else:
-                zeroCounter += 1
-
-        if oneCounter > zeroCounter:
-            for entry in bitList:
-
-                if str(entry)[i] == "1":
-                    updatedBitList.append(entry)
-             
-        elif oneCounter < zeroCounter:
-
-            for entry in bitList:
-                if str(entry)[i] == "0":
-                    updatedBitList.append(entry)
-
-        else:
-            for entry in bitList:
-                if str(entry)[i] == "1":
-                    updatedBitList.append(entry)
-
-        
-        bitList = updatedBitList[:]                
-        updatedBitList = []
-        oneCounter = 0
-        zeroCounter = 0
-
-
-def co2(filename):
-
+    if measure == "co2":
+        zeroWon = "1"
+        oneWon = "0"
+    else:
+        zeroWon = "0"
+        oneWon = "1"
     bitFile = open(filename)
 
     zeroCounter = 0
@@ -125,31 +79,38 @@ def co2(filename):
             else:
                 zeroCounter += 1
 
+        
         if oneCounter < zeroCounter:
             for entry in bitList:
                 #print(entry)
-                if str(entry)[i] == "1":
+                if str(entry)[i] == zeroWon:
                     updatedBitList.append(entry)
              
         elif oneCounter > zeroCounter:
 
             for entry in bitList:
-                if str(entry)[i] == "0":
+                if str(entry)[i] == oneWon:
                     updatedBitList.append(entry)
 
         else:
             for entry in bitList:
-                if str(entry)[i] == "0":
+                if str(entry)[i] == oneWon:
                     updatedBitList.append(entry)
 
         
-        bitList = updatedBitList[:]                
+        bitList = updatedBitList[:]                  
         updatedBitList = []
         oneCounter = 0
         zeroCounter = 0
 
 def main():
-    print("epsiolGamma:",epgam("3.txt"))
-    print("oxCo2: ",int(ox("3.txt")[0], 2)*int(co2("3.txt")[0], 2))
+    
+    
+    co21 = life_support_rating("3.txt", measure = "co2")
+    ox1 = life_support_rating("3.txt", measure = "ox")
+    epsgam = epgam("3.txt")
+    print("epsiolGamma:",epsgam)
+    print("oxCo2: ",int(co21[0], 2) * int(ox1[0], 2))
+    
 
 main() 
