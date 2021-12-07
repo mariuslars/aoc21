a1699202cdf56eee6d3bfd767b06849f6cd4992d
Larsson <riu@ssb.no>
2021-12-07 08:17:57 +0100

    7

---
+++ 7/7.py
@@ -0,0 +1,38 @@
+def crabMissile(file, costly):
+
+
+    def crabEQ(startpos, newpos, costly):
+        changepos = abs(startpos - newpos)
+        if costly:
+            return round(changepos*(1+changepos)/2)
+        else:
+            return changepos
+        
+    crabPos = [int(pos) for pos in open(file).readline().split(",")]
+
+    currentFuel = max(crabPos)
+    fuelCounter = 0
+    
+    horizontalDirection = 1
+    currentLowest = 1e20
+
+    for _ in range(currentFuel):
+
+        for pos in crabPos:
+            
+            fuelCounter += crabEQ(pos, horizontalDirection, costly = costly)
+
+
+        horizontalDirection += 1
+
+        if fuelCounter < currentLowest:
+            currentLowest = fuelCounter
+            
+        fuelCounter = 0
+
+
+    return currentLowest
+
+
+print("Cheap moves: ",crabMissile("7.txt", costly = False))
+print("Expensive moves",crabMissile("7.txt", costly = True))
