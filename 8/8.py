79d254592acecebf475fa3bab0ec2199a1c5c018
Larsson <riu@ssb.no>
2021-12-08 17:03:28 +0100

    fix 9

--- 8/8.py
+++ 8/8.py
@@ -26,53 +26,68 @@ def fixSignal(file):
                 elif len(number) == 3:
                     
                     syv = sortedNumber
-                    print("7: ", number)
+                    
                     displayDict["".join(sorted(number))] = 7
                 elif len(number) == 4:
                     fire = sortedNumber
-                    print("4: ", number)
+                    
                     displayDict["".join(sorted(number))] = 4
                 elif len(number) == 7:
                     atte = sortedNumber
-                    print("8: ", number)
+                    
                     displayDict["".join(sorted(number))] = 8
             else:
                 updatedOutput.append(number)
         updatedOutput2 = []
 
-        for number in updatedOutput:
-
-            
+        for number in updatedOutput:        
             sortedNumber = set(sorted(number))
-            #print(sortedNumber, ". ",syv <= sortedNumber, "lengde: ", len(sortedNumber) == 5)
+            
             if (syv <= sortedNumber) and (len(sortedNumber) == 5):
                 tre = sortedNumber
                 print("3: ", number)
                 displayDict["".join(sorted(number))] = 3
             else:
                 updatedOutput2.append(number)
-       # print("UPDATED1:", updatedOutput)
-       # print("UPDATED2:",updatedOutput2)
 
+        updatedOutput3 = []
         for number in updatedOutput2:
             sortedNumber = set(sorted(number))
             if len(sortedNumber.intersection(fire)) == 2:
                 
                 to = sorted(number)
-                print("2: ", number)
-                displayDict["".join(sorted(number))] = 2
-                sortedNumber = set(sorted(number))
-                #print(len(tre.difference(sortedNumber)) == 1)
-                #print(len(sortedNumber.intersection(fire)))
-                #print("inter: ", sortedNumber.intersection(fire))
-                #print("rawnum: ",number)
-                #print("fir: ",fire)
-                #print("to: ",to)
                 
-                #return("LOL")
+                displayDict["".join(sorted(number))] = 2
+            else:
+                updatedOutput3.append(number)
 
+        updatedOutput4 = []
+        for number in updatedOutput3:
+            sortedNumber = set(sorted(number))
+            if len(atte.difference(sortedNumber)) == 2:
+                
+                fem = sorted(number)
+                
+                displayDict["".join(sorted(number))] = 5
+            else:
+                updatedOutput4.append(number)
+        print(fire)
+        print(sorted(fire))
+        
+        #TODO: DENNE MÅ FIKSES
+        print(updatedOutput4)
+        updatedOutput5 = []
+        for number in updatedOutput4:
+            sortedNumber = set(sorted(number))
+            if len(fire.intersection(updatedOutput4[2]))+2 == 6:
+                
+                ni = sorted(number)
+                
+                displayDict["".join(sorted(number))] = 9
+            else:
+                updatedOutput5.append(number)
+        
 
-    #print("UPDATED2:",updatedOutput2)
     finalNumber = []
     print(displayDict)
     for number in outputSignals[TESTVAR]:
@@ -97,18 +112,16 @@ e   f\
 
 #0 aller sist nulll.difference(set.intersection(to, tre, fire, fem))
 
-#2 ETTER IDENTIFISERING AV 3: = 3 difference av 2 == 1, feks hvis 2 = {"a", "b", "c", "d"} og 3 = {"a", "b", "c", "e"}, da er 3.difference(2) == 1 (viktig med 3 først)
-
-
-#5 Etter identifisering av 2 og 3: 8 difference av 5 == 2, 8.difference(2)
 #6 Etter identifisering av 5, 8, 9: set.union(fem,atte.difference(ni))
 
 #9 = innholdet i 4 + 2 bokstaver til #ETTER IDENTIFISERING AV 9
 
 
 #1 direkte identifisert
+#2 ETTER IDENTIFISERING AV 3: = 3 difference av 2 == 1, feks hvis 2 = {"a", "b", "c", "d"} og 3 = {"a", "b", "c", "e"}, da er 3.difference(2) == 1 (viktig med 3 først)
 #3 ETTER IDENTIFISERING AV 7: = innholdet i 7 + 2 bokstaver til 
 #4 direkte identifisert
+#5 Etter identifisering av 2 og 3: 8 difference av 5 == 2, 8.difference(2)
 #7 direkte identifisert
 #8 direkte identifisert
 
