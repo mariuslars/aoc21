b62875a9599c153e8626bc8ee8037202d54143fd
marius <marius.runningen.larsson@ssb.no>
2021-12-08 22:06:19 +0100

    8 brute force solution

--- 8/8.py
+++ 8/8.py
@@ -7,22 +7,29 @@ def fixSignal(file):
 
         outputSignals.append(signal.split("|")[1].strip().split(" "))
         inputSignals.append(signal.split("|")[0].strip().split(" "))
-    
-    TESTVAR = 3
+
     uniqueCounter = 0
+    for input in outputSignals:
+        for number in input:
+        #s#ortedNumber = set(sorted(number))
+            if len(number) in [2, 3, 4, 7]:
+                uniqueCounter+=1
+
+    
     updatedOutput = []
     displayDict = {}
+    dictList = []
     scoreCounter = 0
-    for output in [inputSignals[TESTVAR]]:
+    for output in inputSignals:
         for number in output:
             
             sortedNumber = set(sorted(number))
-            if len(number) in [2, 3, 4, 7]:
-                uniqueCounter+=1
+            if len(sortedNumber) in [2, 3, 4, 7]:
+                #uniqueCounter+=1
                 if len(number) == 2:
-                    en = sortedNumber
+
                     
-                    print("1: ")
+                    
                     displayDict["".join(sorted(number))] = 1
                 elif len(number) == 3:
                     
@@ -45,7 +52,7 @@ def fixSignal(file):
             sortedNumber = set(sorted(number))
             
             if (syv <= sortedNumber) and (len(sortedNumber) == 5):
-                tre = sortedNumber
+
                 
                 displayDict["".join(sorted(number))] = 3
             else:
@@ -85,51 +92,60 @@ def fixSignal(file):
             else:
                 updatedOutput5.append("".join(sorted(number)))
 
-    seks = set.union(set(fem),atte.difference(set(ni)))
+        seks = set.union(set(fem),atte.difference(set(ni)))
    
     
-    displayDict["".join(sorted(seks))] = 6
+        displayDict["".join(sorted(seks))] = 6
 
-    updatedOutput5.remove("".join(sorted(seks)))
+        updatedOutput5.remove("".join(sorted(seks)))
 
-    zero = updatedOutput5[0]
-    displayDict[zero] = 0
-    
+        zero = updatedOutput5[0]
+        displayDict[zero] = 0
+
+        dictList.append(displayDict)
+        displayDict = {}
+        updatedOutput5 = []
+        updatedOutput4 = []
+        updatedOutput3 = []
+        updatedOutput2 = []
+        updatedOutput = []
+
+        
     finalNumber = []
 
-    for number in outputSignals[TESTVAR]:
+
+
+    for indexer in range(len(dictList)):
         
-        sortnum = "".join(sorted(number))
-        try:
-            finalNumber.append(str(displayDict[sortnum]))
-        except:
-            continue
-    scoreCounter += int("".join(finalNumber))
+        numberList = outputSignals[indexer]
+        
+        for number in numberList:
+  
+            sortnum = "".join(sorted(number))
+            
 
-    return scoreCounter
-print(fixSignal("8_ex.txt"))
+            try: 
+                finalNumber.append(str(dictList[indexer][sortnum]))
+            except:
+                continue
+        #print(int("".join(finalNumber)))
+        scoreCounter += int("".join(finalNumber))   
+        finalNumber = []
+    
+    
+    return [uniqueCounter, scoreCounter]
+print(fixSignal("8.txt"))
 
-" a\
-b   c\
-  d\
-e   f\
-  g"
 
-#f = 9 unike
-#e = 4 unike
+
 
 #0 aller sist nulll.difference(set.intersection(to, tre, fire, fem))
-
-#6 Etter identifisering av 5, 8, 9: set.union(fem,atte.difference(ni))
-
-
-
-
 #1 direkte identifisert
 #2 ETTER IDENTIFISERING AV 3: = 3 difference av 2 == 1, feks hvis 2 = {"a", "b", "c", "d"} og 3 = {"a", "b", "c", "e"}, da er 3.difference(2) == 1 (viktig med 3 først)
 #3 ETTER IDENTIFISERING AV 7: = innholdet i 7 + 2 bokstaver til 
 #4 direkte identifisert
 #5 Etter identifisering av 2 og 3: 8 difference av 5 == 2, 8.difference(2)
+#6 Etter identifisering av 5, 8, 9: set.union(fem,atte.difference(ni))
 #7 direkte identifisert
 #8 direkte identifisert
 #9 = innholdet i 4 + 2 bokstaver til #ETTER IDENTIFISERING AV 9
